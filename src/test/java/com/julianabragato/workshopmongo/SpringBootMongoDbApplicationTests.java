8cc13609a7730e07f902e7060414d12c89c9ba7d
Juliana Bragato <july.plis@gmail.com>
2023-10-25 12:52:16 -0300

    Project Start.

---
+++ src/test/java/com/julianabragato/workshopmongo/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianabragato.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
