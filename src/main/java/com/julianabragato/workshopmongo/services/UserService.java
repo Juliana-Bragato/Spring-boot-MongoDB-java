909b473782144001a4a8336a3fde76c35cdbbecf
Juliana Bragato <july.plis@gmail.com>
2023-10-26 15:20:24 -0300

    Inserção de usuário com POST.

--- src/main/java/com/julianabragato/workshopmongo/services/UserService.java
+++ src/main/java/com/julianabragato/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.julianabragato.workshopmongo.domain.User;
+import com.julianabragato.workshopmongo.dto.UserDTO;
 import com.julianabragato.workshopmongo.repository.UserRepository;
 import com.julianabragato.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
